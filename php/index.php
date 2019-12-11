7a3f38921f8fdf0a6a8ebbf2f1a1f299c1207c45
Jitendra Adhikari <jiten.adhikary@gmail.com>
2019-12-11 19:21:27 +0700

    chore: add adminer url

--- php/index.php
+++ php/index.php
@@ -38,4 +38,7 @@ foreach (array_chunk($extensions, 4) as $exts) {
 echo PHP_SAPI === 'cli'
     ? "\nSource code: https://github.com/adhocore/docker-lemp\n\n"
     : "</pre>\n\n"
-        . 'Source code: <a href="https://github.com/adhocore/docker-lemp" target="_blank">adhocore/docker-lemp</a>';
+        . 'Source code: <a href="https://github.com/adhocore/docker-lemp" target="_blank">adhocore/docker-lemp</a>'
+        . ' | Adminer: <a href="/adminer?server=127.0.0.1%3A3306&username=root" target="_blank">mysql</a>, '
+        . ' <a href="/adminer?pgsql=127.0.0.1%3A5432&username=postgres" target="_blank">postgres</a>'
+        . "\n";
